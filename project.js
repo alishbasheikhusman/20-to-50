68e2383c5d0ae94e06e01fd131e24283518ab78e
alishbasheikhusman <152896627+alishbasheikhusman@users.noreply.github.com>
2024-01-06 14:58:08 +0500

    Add files via upload
    done

---
+++ project.js
@@ -0,0 +1,64 @@
+// concat
+
+// var a = ["alishba","bisma","usama","fayyz"];
+// var b = ["khan","sheikh","shahb"];
+// var c = a.concat(b);
+// console.log(c);
+
+// join
+// var a = ["alishba","bisma","usama","fayyz"];
+// var b = a.join('-');
+// console.log(b);
+ 
+
+// shift
+
+// var a = ["alishba","bisma","usama","fayyz"];
+//      a.unshift('hunain');
+// console.log(a);
+
+// slice
+
+
+//  var a = ["a","b","c","d","e","f","g"];
+//  var b = a.slice(-0,-3);
+// console.log(b);
+
+
+// loop start
+
+
+    // for(let index = 2; index < 100; index =index + 2){
+
+    //     console.log(index);
+
+
+    // }
+
+// for(var i = 0; i <= 10; i++){
+//     console.log(i);
+// }
+
+// var password = 'alishba.1234';
+// var hiddenPassword = '*'.repeat(password.length);
+// console.log(hiddenPassword);
+
+//  var a = 3.4;
+//  var b = Math.round(a);
+// console.log(b);
+
+// var a = 46.1;
+// var b = Math.ceil(a);
+// console.log(b);
+
+// var a = 47.7;d 
+// var b = Math.floor(a);
+// console.log(b);
+
+// var a = "karachi";
+// var letter1 = a.slice(0,1);
+// var otherLetter = a.slice(1);
+// letter1 = otherLetter.toLocaleUpperCase();
+// letter1 = otherLetter.toLocaleLowerCase();
+// var word = letter1 + otherLetter;
+// console.log(word);
